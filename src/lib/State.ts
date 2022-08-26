9c18245121a88a74889c9fe808e2341e138b881e
IIvexII <mrh4ck3r1738@gmail.com>
2022-08-26 18:37:10 +0500

    Refactor: State.ts

--- src/lib/State.ts
+++ src/lib/State.ts
@@ -3,36 +3,27 @@ interface stateData {
 }
 // State of input
 export class State {
-  private inputs?: NodeListOf<HTMLInputElement>;
+  private inputs: NodeListOf<HTMLInputElement>;
 
   constructor(selector: string) {
-    const inputs: NodeListOf<HTMLInputElement> =
-      document.querySelectorAll(selector);
-    if (inputs) {
-      this.inputs = inputs;
-    }
+    this.inputs = document.querySelectorAll(selector);
   }
+  // Set the state of the input fields according to the stored.
   set() {
-    if (this.inputs) {
-      // Set the value of each input field.
-      this.inputs.forEach((input: HTMLInputElement, index: number) => {
-        const localValue = localStorage.getItem(`input${index}`);
-        if (localValue) {
-          input.value = localValue;
-        }
-      });
-    } else {
-      console.log('There is an error while setting input fields.');
-    }
+    // Set the value of each input field.
+    this.inputs.forEach((input: HTMLInputElement, index: number) => {
+      const localValue = localStorage.getItem(`input${index}`);
+      if (localValue) {
+        input.value = localValue;
+      }
+    });
   }
   get(): stateData {
     const state: stateData = {};
 
-    if (this.inputs) {
-      this.inputs.forEach((input: HTMLInputElement, index: number) => {
-        state[`input${index}`] = input.value;
-      });
-    }
+    this.inputs.forEach((input: HTMLInputElement, index: number) => {
+      state[`input${index}`] = input.value;
+    });
     return state;
   }
 }
